dfa44346aace65806a123abf80be1d1a274c774e
Steinar V. Kaldager <steinarvkaldager@gmail.com>
2013-07-09 18:32:23 +0200

    code that should theoretically work
    has not been tested at all, written on bus

--- src/main.py
+++ src/main.py
@@ -22,6 +22,8 @@ import pymunk
 
 import blocks
 
+from world import World
+
 class Ship (physics.Thing):
     def __init__(self, sim, block_structure, layer, position, sprite_name = "player.png", mass = 1.0, moment = 1.0, **kwargs):
         super( Ship, self ).__init__( sim, physics.zero_shape_centroid( block_structure.create_collision_shape() ), mass, moment, **kwargs )
@@ -142,6 +144,123 @@ def create_bullet_thing(sim, image, shooter):
     rv.velocity = shooter.velocity + shooter.direction * (speed)
     rv.angle_radians = degrees_to_radians( shooter.angle_degrees + 90.0 )
     return rv
+
+class MainWorld (World):
+    def __init__(self, resolution = (1300,1000), use_pygame = False, **kwargs):
+        super( MainWorld, self ).__init__(self, **kwargs)
+        self.setup_graphics( resolution )
+        self.setup_game()
+        self.setup_input()
+        self.camera.following = self.player
+        self.main_layer.camera = self.camera
+        if use_pygame:
+            self.setup_pygame( resolution )
+            self.display.add_anonymous_hook( self.update_pygame )
+        self.pre_physics.add_anonymous_hook( self.update_physics_objects )
+        self.pre_display.add_anonymous_hook( self.update_display_objects )
+        self.pre_physics.add_anonymous_hook( self.update_camera )
+        self.display.add_anonymous_hook( self.scene.update )
+        self.pre_physics.add_hook( self.player, self.player.update )
+        self.pre_physics.add_hook( self.enemy, lambda dt : ai_seek_target( dt, self.enemy, self.player, self.shoot_bullet ) )
+        self.pre_physics.add_hook( self.enemy, self.enemy.update )
+        self.physics.add_hook( self.sim.tick )
+        self.scene.schedule( self.update_everything )
+        self.window.run( self.scene )
+    def update_everything(self, dt):
+        self.tick( dt )
+        self.display_update()
+    def update_camera(self, dt):
+        self.camera.update( dt )
+    def setup_pygame(self, resolution):
+        pygame.init()
+        self.screen = pygame.display.set_mode( resolution )
+    def setup_graphics(self, resolution):
+        self.window = graphics.Window( resolution )
+        self.camera = graphics.Camera( window )
+        self.scene = graphics.Scene( window )
+        graphics.Layer( self.scene, cocos.layer.ColorLayer( 0, 0, 0, 1 ) )
+        for i in range(8):
+            graphics.Layer( self.scene, graphics.BackgroundCocosLayer( self.camera, 10.0 + 0.5 * i, "starfield{0}.png".format(i) ) )
+        self.main_layer = graphics.Layer( self.scene )
+        self.main_layer.cocos_layer.position = self.camera.offset()
+    def setup_game(self):
+        self.sim = physics.PhysicsSimulator( timestep = None )
+        self.player = create_ship_thing( self.sim, self.main_layer, (0,0) )
+        self.enemy = create_ship_thing( self.sim, self.main_layer, (500,0), big = True )
+        self.img_square = pyglet.image.load( "element_red_square.png" )
+        self.img_bullet = pyglet.image.load( "laserGreen.png" )
+        self.batch = cocos.batch.BatchNode()
+        self.main_layer.cocos_layer.add( self.batch )
+        self.display_objects = []
+        for i in range(200):
+            cols = "red", "purple", "grey", "blue", "green", "yellow"
+            sq = create_square_thing( window.sim, None, (100,0), self.img_square )
+            sq.position = (random.random()-0.5) * 4000, (random.random()-0.5) * 4000
+            sq.angle_radians = random.random() * math.pi * 2
+            sq.mylabel = sq.position
+            sq.velocity = (300,10)
+            self.batch.add( sq.sprite.cocos_sprite )
+            self.display_objects.append( sq.sprite )
+        self.sim.space.add_collision_handler( collision_type_main, collision_type_bullet, self.collide_general_with_bullet )
+    def setup_input(self):
+        input_layer = graphics.Layer( scene, gameinput.CocosInputLayer() )
+        input_layer.cocos_layer.set_key_press_hook( key.SPACE, player.on_fire_key )
+        for k in (key.LEFT, key.RIGHT, key.UP, key.DOWN):
+            input_layer.cocos_layer.set_key_hook( k, player.on_controls_state )
+        input_layer.cocos_layer.set_key_hook( k, player.on_controls_state )
+        input_layer.cocos_layer.set_key_hook( key.LSHIFT, player.on_controls_state )
+        input_layer.cocos_layer.set_key_press_hook( key.space, partial( self.shoot_bullet, self.player ) )
+    def shoot_bullet(self, shooter):
+        if shooter.may_fire():
+            sq = create_bullet_thing( self.sim, self.img_bullet, shooter )
+            sq.ttl = 1.5
+            self.display_objects.append( sq.sprite )
+            self.batch.add( sq.sprite.cocos_sprite )
+            self.physics_objects.append( sq )
+            shooter.fired()
+    def update_physics_objects(self, dt):
+        tbr = []
+        for o in self.physics_objects:
+            o.ttl -= dt
+            if o.ttl <= 0.0:
+                o.kill()
+            if not o.alive:
+                tbr.append( o )
+        for o in tbr:
+            self.physics_objects.remove(o)
+    def update_pygame(self):
+        self.screen.fill( pygame.color.THECOLORS[ "black" ] )
+        draw_space( self.screen, self.sim )
+        pygame.display.flip()
+    def update_display_objects(self):
+        x, y = self.camera.focus
+        hw, hh = 0.5 * self.window.width, 0.5 * self.window.height
+        screen_slack = 100.0
+        beyond_slack = screen_slack + 500.0
+        screen_bb = pymunk.BB(x-hw-screen_slack,y-hh-screen_slack,x+hw+screen_slack,y+hh+screen_slack)
+        big_bb = pymunk.BB(x-hw-beyond_slack,y-hh-beyond_slack,x+hw+beyond_slack,y+hh+beyond_slack)
+        onscreen = set(self.sim.space.bb_query( screen_bb ))
+        for shape in onscreen:
+            shape.thing.sprite.update()
+        prech = set(self.batch.get_children())
+        for spr in self.display_objects:
+            if all((shape not in onscreen for shape in spr.thing.shapes)):
+                if spr.cocos_sprite in prech:
+                    self.batch.remove( spr.cocos_sprite )
+            else:
+                if spr.cocos_sprite not in prech:
+                    self.batch.add( spr.cocos_sprite )
+    def collide_general_with_bullet(self, space, arbiter ):
+        anything, bullet = arbiter.shapes
+        bullet.thing.ttl = min( bullet.thing.ttl, 0.05 )
+        return True
+        
+        
+
+
+        
+        
+
     
 def main():
     use_pygame = False
