a052cf5d772ef1805f78c0d4d93dd62b8ca5ba5e
Steinar V. Kaldager <steinarvkaldager@gmail.com>
2013-07-27 12:08:14 +0200

    altered shaders to better show shading

--- src/shaded-block.v.glsl
+++ src/shaded-block.v.glsl
@@ -15,10 +15,10 @@ varying vec4 v_tint;
 void main() {
     float aspect = 640.0 / 480.0;
     float t = fade_factor;
-    float a = angle + t * 1.0;
+    float a = angle + t * 0.2;
     rcosa = cos(a);
     rsina = sin(a);
-    vec2 rp = com_position + vec2(t*0.05,t*0.1)*1.0 + vec2(position_offset.x * rcosa - position_offset.y * rsina, position_offset.x * rsina + position_offset.y * rcosa );
+    vec2 rp = com_position + vec2(t*0.05,t*0.1)*0.1 + vec2(position_offset.x * rcosa - position_offset.y * rsina, position_offset.x * rsina + position_offset.y * rcosa );
     gl_Position = vec4( rp.x / aspect, rp.y, 0.0, 1.0 );
     texcoord = attr_texcoord;
     v_tint = tint;
